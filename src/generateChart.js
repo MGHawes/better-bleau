5242fe54f378b27c544602de3eefea758a3c556d
Matt Hawes <mhawes@palantir.com>
2019-12-30 23:52:09 +0000

    Chart styling tweaks

--- src/generateChart.js
+++ src/generateChart.js
@@ -1,82 +1,155 @@
 import ApexCharts from 'apexcharts';
-import { flatMap, countBy, map, sortBy } from 'lodash';
+import { flatMap, countBy, map, sortBy, mapValues, reverse } from 'lodash';
+
+const NUM_CLIMB_TYPES_TO_SHOW = 10
+const APPROXIMATE_LABEL_WIDTH_IN_PIXELS = 50
+const SMALLEST_VISIBLE_LABEL_WIDTH_IN_PIXELS = 25
 
 export function generateChart () {
-    const [leftColumn, rightColumn] = getRightAndLeftColumns()
-  
-    const climbContainerElementsByGrade = groupByGrade(leftColumn)
-    const climbs = flatMap(
-      climbContainerElementsByGrade,
-      (elements, grade) => flatMap(elements, elem => getClimbTypes(elem).map(type => ({ type, grade })))
-    )
-  
-    const climbTypeCounts = countBy(climbs, climb => climb.type)
-  
-    const newNode = document.createElement('div');
-    newNode.className = "row"
-    if (rightColumn.children.length < 2) {
-      return
-    }
-    rightColumn.insertBefore(newNode, rightColumn.children[1]);
-  
-    const data = map(climbTypeCounts, (count, type) => [type, count])
-    const sortedData = sortBy(data, v => -v[1])
-    const top15Values = sortedData.slice(0, 15)
-  
-    const options = {
-        chart: {
-          type: 'bar'
+  const [leftColumn, rightColumn] = getRightAndLeftColumns()
+
+  const climbContainerElementsByGrade = groupByGrade(leftColumn)
+  const climbTypesByGrade = mapValues(
+    climbContainerElementsByGrade,
+    elements => flatMap(elements, getClimbTypes)
+  )
+  const topClimbTypes = getTopNClimbTypes(climbTypesByGrade, NUM_CLIMB_TYPES_TO_SHOW)
+  const maxCount = topClimbTypes[0][1]
+
+  const climbTypeCountsByGrade = mapValues(
+    climbTypesByGrade,
+    climbTypes => countBy(climbTypes)
+  )
+
+  const chartSeries = map(
+    climbTypeCountsByGrade,
+    (countsByType, grade) => ({
+      name: grade.toString(),
+      data: topClimbTypes.map(([type, _]) => type.toString() in countsByType ? countsByType[type] : 0)
+    })
+  )
+
+  const newElement = createChartElement(rightColumn)
+  const chartBarslength = newElement - APPROXIMATE_LABEL_WIDTH_IN_PIXELS
+  const options = {
+      chart: {
+        type: "bar",
+        height: 350,
+        stacked: true,
+        toolbar: {
+          show: true
         },
-        series: [{
-          name: 'Number of Problems',
-          data: top15Values.map(v => v[1])
-        }],
-        xaxis: {
-          categories: top15Values.map(v => v[0])
-        }
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: val => 
+          // HACK HACK: Labels render even when there isn't enough room in the bar
+          (val / maxCount) > (SMALLEST_VISIBLE_LABEL_WIDTH_IN_PIXELS / chartBarslength)
+            ? val.toString() : "",
+      },
+      series: reverse(chartSeries),
+      xaxis: {
+        categories: topClimbTypes.map(([type, _]) => toTitleCase(type)),
       }
-        
-    const chart = new ApexCharts(newNode, options);
-    
-    chart.render();
-  }
-  
-  const getRightAndLeftColumns = () => {
-    const mainContent = document.querySelector("main div.container")
-    if (mainContent == null) {
-      throw "Couldn't find main container element"
     }
-    const rightColumn = mainContent.querySelector(".pull-right")
-    const leftColumn = mainContent.querySelector(":not(.pull-right).col-md-6")
-    if (rightColumn == null || leftColumn == null) {
-      throw "Couldn't find left right columns";
-    }
-  
-    return [leftColumn, rightColumn]
+      
+  const chart = new ApexCharts(newElement, options);
+
+  chart.render();
+}
+
+const createChartElement = rightColumn => {
+  const chartElement = document.createElement('div');
+  chartElement.className = "row"
+  chartElement.style.width = "100%"
+  if (rightColumn.children.length < 2) {
+    return
   }
+  rightColumn.insertBefore(chartElement, rightColumn.children[1]);
   
-  const isClimbContainerElement = elem => elem.tagName === "DIV" && elem.className === "vsr"
-  const isGradeHeadingElement = elem => elem.tagName === "H4"
-  
-  const groupByGrade = leftColumn => {
-    const climbContainersByGrades = {}
-    let currentGrade
-    for (const elem of leftColumn.children) {
-      if (isGradeHeadingElement(elem)) {
-        currentGrade = elem.innerText.trim()
+  return chartElement
+}
+
+const getTopNClimbTypes = (climbTypesByGrade, n) => {
+  const allClimbTypeCounts = countBy(flatMap(climbTypesByGrade))
+  const topNClimbTypes = sortBy(
+    map(allClimbTypeCounts, (v, k) => [k, v]),
+    ([k, v]) => -v, 
+  ).slice(0, n)
+
+  return topNClimbTypes;
+}
+
+const getRightAndLeftColumns = () => {
+  const mainContent = document.querySelector("main div.container")
+  if (mainContent == null) {
+    throw "Couldn't find main container element"
+  }
+  const rightColumn = mainContent.querySelector(".pull-right")
+  const leftColumn = mainContent.querySelector(":not(.pull-right).col-md-6")
+  if (rightColumn == null || leftColumn == null) {
+    throw "Couldn't find left right columns";
+  }
+
+  return [leftColumn, rightColumn]
+}
+
+const isClimbContainerElement = elem => elem.tagName === "DIV" && elem.className === "vsr"
+const isGradeHeadingElement = elem => elem.tagName === "H4"
+
+const groupByGrade = leftColumn => {
+  const climbContainersByGrades = {}
+  let currentGrade
+  for (const elem of leftColumn.children) {
+    if (isGradeHeadingElement(elem)) {
+      currentGrade = getGrade(elem.innerText)
+
+      if (climbContainersByGrades[currentGrade] == null) {
         climbContainersByGrades[currentGrade] = []
-      } else if (isClimbContainerElement(elem) && currentGrade != null) {
-        climbContainersByGrades[currentGrade].push(elem)
       }
+    } else if (isClimbContainerElement(elem) && currentGrade != null) {
+      climbContainersByGrades[currentGrade].push(elem)
     }
-    return climbContainersByGrades
   }
-  
-  const getClimbTypes = elem => {
-    const typesElem = elem.querySelector(".btype")
-    if (typesElem == null) {
-      return [];
-    }
-    return typesElem.innerText.split(",").map(t => t.trim())
+
+  return climbContainersByGrades
+}
+
+const getGrade = gradeText => {
+  const grade = Number(gradeText.trim().slice(0, 1))
+  if (isNaN(grade)) {
+    return "Unknown"
   }
-  
+  if (grade < 5) {
+    return "< 5"
+  }
+  return `${grade}s`
+}
+
+const getClimbTypes = elem => {
+  const typesElem = elem.querySelector(".btype")
+  if (typesElem == null) {
+    return [];
+  }
+  return typesElem.innerText.split(",").map(parseClimbType)
+}
+
+const parseClimbType = typeString => {
+  const type = typeString.trim()
+  if (type.search(/[Tt]raverse/) != -1) {
+    return "traverse"
+  }
+  return type
+}
+
+function toTitleCase(str) {
+  return str.replace(
+      /\w\S*/g,
+      txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  );
+}
